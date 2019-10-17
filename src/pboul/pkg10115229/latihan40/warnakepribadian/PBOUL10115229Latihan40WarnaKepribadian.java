7cb601e5d06efa000962eb72c9667aabfcb4a645
Petani Kode <muhammadlukman09@gmail.com>
2019-10-17 18:12:51 +0700

    project empatpuluh

---
+++ src/pboul/pkg10115229/latihan40/warnakepribadian/PBOUL10115229Latihan40WarnaKepribadian.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author Lukman
+ */
+public class PBOUL10115229Latihan40WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/pboul/pkg10115229/latihan40/warnakepribadian/Warna.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author Lukman
+ */
+public class Warna {
+    
+}
